55637308346ba898246dc013718802d4df6a9f69
Aman Rastogi <arastogi2810@gmail.com>
2025-05-28 14:39:59 +0530

    Base Setup: Root & Project & Container Module Init

---
+++ container-server/src/main/java/self/boltclone/containerserver/ContainerServerApplication.java
@@ -0,0 +1,13 @@
+package self.boltclone.containerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContainerServerApplication.class, args);
+    }
+
+}
---
+++ project-server/src/main/java/self/boltclone/projectserver/ProjectServerApplication.java
@@ -0,0 +1,13 @@
+package self.boltclone.projectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectServerApplication.class, args);
+    }
+
+}
